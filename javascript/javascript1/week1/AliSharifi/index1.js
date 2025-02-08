ed2e438e6e1bf378c0f2afafeffdbab1d27cc274
Ali <mohammadali.sharifi@rocketmail.com>
2025-02-08 20:17:35 +0100

    Rename js file

--- javascript/javascript1/week1/AliSharifi/index1.js
+++
@@ -1,7 +0,0 @@
-function AgeIfy(yearOfBirth, yearFuture) {
-  const age = yearFuture - yearOfBirth;
-  consolePrint = "You will be " + age + " years old in " + yearFuture + ".";
-  return consolePrint;
-}
-
-console.log(AgeIfy(1988, 2045));
