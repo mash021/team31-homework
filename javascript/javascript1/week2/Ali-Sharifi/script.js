a99eb3ce491e784febee6d027ca2cb8543040974
Ali <mohammadali.sharifi@rocketmail.com>
2025-02-19 16:02:11 +0100

    Format code with Prettier

--- javascript/javascript1/week2/Ali-Sharifi/script.js
+++ javascript/javascript1/week2/Ali-Sharifi/script.js
@@ -1,14 +1,9 @@
 function getFullname(firstname, surname) {
-    return firstname + " " + surname;
-  }
-  
-  const fullname1 = getFullname("Ali", "Sharifi");
-  const fullname2 = getFullname("Christopher", "Hansen");
-  
-  console.log(fullname1); // "Ali Sharifi"
-  console.log(fullname2); // "Christopher Hansen"
-  
+  return firstname + " " + surname;
+}
 
+const fullname1 = getFullname("Ali", "Sharifi");
+const fullname2 = getFullname("Christopher", "Hansen");
 
-
-  
+console.log(fullname1); // "Ali Sharifi"
+console.log(fullname2); // "Christopher Hansen"
