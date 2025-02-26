126660d3036087b6a12f1e8a364157d559d7f668
Ali <mohammadali.sharifi@rocketmail.com>
2025-02-26 14:46:16 +0100

    CodeWars  home work is resolved

--- javascript/javascript1/week4/aliSharifi/vowelCount.js
+++ javascript/javascript1/week4/aliSharifi/vowelCount.js
@@ -14,7 +14,7 @@ function countVowels(str) {
 console.log(countVowels("Hack your future is awesome"));
 
 
-//digit dogits
+//Digit*Digit
 function squareDigits(num) {
     return parseInt(
         num.toString()
@@ -27,3 +27,12 @@ function squareDigits(num) {
 
 console.log(squareDigits(9349)); 
 console.log(squareDigits(865)); 
+
+//7kyu Highest and Lowest
+function minMax(arr) {
+    return [Math.min(...arr), Math.max(...arr)];
+}
+
+console.log(minMax([1, 2, 3, 4, 5])); // [1, 5]
+console.log(minMax([2334454, 5])); // [5, 2334454]
+console.log(minMax([1])); // [1, 1]
